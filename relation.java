81fa5fd40f09ef208dd5ea72f9ec4c5f8d382c4e
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-12-23 22:26:36 +0530

    ternary

---
+++ relation.java
@@ -0,0 +1,37 @@
+class relation{
+	public static void main(String args[]){
+		// relational operator
+		
+		// less than operator
+		int a=10;
+		int b=12;
+		System.out.println(a<b);
+		System.out.println(b<a);
+		
+		// gretar than operator
+	    System.out.println(a>b);
+	    System.out.println(b>a);
+		
+		// lessthan or eqal operator
+		int c=12;
+		int d=12;
+		System.out.println(c<=d);
+		System.out.println(d<=c);
+		
+	     //gretar than or eqal operator
+		System.out.println(c>=d);
+		System.out.println(d>=c);
+		
+		// not eqal operator
+		System.out.println(c!=d);
+		System.out.println(5!=10);
+		
+		//comparision operator
+		System.out.println(c==d);
+		System.out.println(14==14);
+		System.out.println("hello"=="hello");
+		
+		
+		
+	}
+}
---
+++ ternaryoperator.java
@@ -0,0 +1,11 @@
+class ternary{
+	public static void main(String args[]){
+		// ternary operator
+		int a=5;
+		int b=6;
+		String a1=a> b ?  "true Block"     :  "false block";
+		/* ternary operator contain two block one is true block and false block
+		   basec on the condition it execute the statement */
+		System.out.println(a1);
+	}
+}
