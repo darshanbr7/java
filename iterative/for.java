e5dea209b855366e08737bb6d3ffcd024c6519d0
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-12-24 18:33:10 +0530

    iterative

---
+++ iterative/for.java
@@ -0,0 +1,8 @@
+class forloop {
+	public static void main(String args[]){
+		for(int i=0;i<10;i++){
+			System.out.println(i);
+		}
+	}
+}
+/* in for loop when we initilizing the value also we declare the what data type we declaring*/
