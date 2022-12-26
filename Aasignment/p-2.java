e3e972283f991f16567f9bddab20dbf8c6f7e9a2
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-12-26 13:50:14 +0530

    java

---
+++ Aasignment/p-2.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+class problem{
+	public static void main(String atgs[]){
+		Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+         System.out.print("Enter first number- ");  
+         int a= sc.nextInt(); 
+      		 if(a%2==0){
+				 System.out.print( a+"  is even number");
+			 } else {
+				 System.out.print( a+ " is Odd number");
+			 }
+		 
+		 
+	}
+}
