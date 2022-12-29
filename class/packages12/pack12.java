a1c8d4e1c7ff7ab6bc6dbd243782aad3a2ce1d15
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-12-29 20:15:59 +0530

    java

---
+++ class/packages12/pack12.java
@@ -0,0 +1,17 @@
+// package levele Acess modipackage pack;
+package pack;
+class pack12{
+	public  void m1(){
+		System.out.println("m1 method");
+	}
+	private void m2(){
+        System.out.println("Class A - m1()");
+    }
+	protected void m3(){
+        System.out.println("Class A - m1()");
+    }
+	void m4(){
+        System.out.println("Class A - m1()");
+    }
+	
+}
---
+++ class/packages12/pack13.java
@@ -0,0 +1,13 @@
+package pack;
+class pack13{
+	public static void main(String args[]){
+		 System.out.println("java packages");
+		 // pack12 a1=new pack12();
+		 // a1.m1();
+		  pack12 a1= new pack12();
+           a1.m1();
+           //a1.m2();
+           a1.m3();
+           a1.m4();
+	}
+}
