63605f53a258cf6496c14e79b311b546208812c3
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-12-30 10:18:54 +0530

    public -default

---
+++ Acessmodifier/public/p1.java
@@ -0,0 +1,11 @@
+package pack;
+public class p1{
+	public static void main(String args[]){
+		 p1 a=new p1();
+		 a.m1();
+		
+	}
+	public  void m1(){
+		System.out.println("public Acesss modifier  m1- method");
+	}
+}
---
+++ Acessmodifier/public/p2.java
@@ -0,0 +1,7 @@
+package pack;
+public class p2{
+	public static void main(String args[]){
+		 p1 b=new p1();
+		 b.m1();
+	}
+}
