2b6842a0cd37830a047cb0bbfac8552d233dc149
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-12-30 10:53:00 +0530

    private

---
+++ Acessmodifier/private/p1.java
@@ -0,0 +1,10 @@
+package pack;
+  public class p1{
+	public static void main(String args[]){
+		 p1 a=new p1();
+		 a.m1();
+	}
+	private void m1(){
+		System.out.println("private Acess modifier    m1 method");
+	}
+}
---
+++ Acessmodifier/private/p2.java
@@ -0,0 +1,7 @@
+package pack;
+class p2{
+	public static void main(String args[]){
+   p1 b=new p1();
+   b.m1();
+	}
+}
