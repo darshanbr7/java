2f5387e3a9088a390a833876e4c3cd58b0f59279
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2022-12-30 12:51:20 +0530

    protected

---
+++ Acessmodifier/protected/p1.java
@@ -0,0 +1,15 @@
+package pack;
+ public class p1{
+	public static void main(String args[]){
+		 
+	}
+	protected void  m1(){
+		System.out.println("protected Acessmodifier  m1 method");
+	}
+    protected void  m2(){
+		System.out.println("protected Acessmodifier  m2 method");
+	}
+	protected void  m3(){
+		System.out.println("protected Acessmodifier  m3 method");
+	}
+}
---
+++ Acessmodifier/protected/p2.java
@@ -0,0 +1,15 @@
+package pack;
+ class p2 extends p1{
+	 public static void main(String args[]){
+      p2 d=new p2();
+		  d.m1();
+		  d.m2();
+		  d.m3();
+	 }
+	/* class p4 extends p1{
+		  p4 d=new p4();
+		  d.m1();
+		  d.m2();
+		  d.m3();
+	 }*/
+ }
