556323fbc168684c2b3afd990780b2bc1ac6ed00
darshanbr7 <115538913+darshanbr7@users.noreply.github.com>
2023-01-24 20:12:52 +0530

    java

---
+++ arraylist/src/arraylist/arr.java
@@ -0,0 +1,13 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class arr {
+public static void main(String[] args) {
+	ArrayList a=new ArrayList();
+	a.add(10);
+	a.add(12);
+	a.add("darshan");
+	System.out.println(a);
+}
+}
